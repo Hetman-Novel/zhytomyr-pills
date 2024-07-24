a8905827ca04d129838e1c665f1120419c3c217a
Роман <69018645+Hetman-Novel@users.noreply.github.com>
2024-07-24 21:10:34 +0300

    Add files via upload

--- files/select2/js/init.js
+++ files/select2/js/init.js
@@ -1,3 +1,8 @@
 $(function() {
-   $('.product-select2').select2();
+   $('.product-select2').select2({
+      theme: 'product-page-select2'
+   });
+   $('.checkout-select2').select2({
+      theme: 'checkout-select2'
+   });
 });
