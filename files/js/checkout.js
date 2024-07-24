b7a46e0e271716bce996d8daa0b44e18e17bc4c6
Роман <69018645+Hetman-Novel@users.noreply.github.com>
2024-07-24 21:11:38 +0300

    Add files via upload

---
+++ files/js/checkout.js
@@ -0,0 +1,81 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+   let bankTransferChecked = document.getElementById('bank-transfer-checked')
+   if (bankTransferChecked) {
+
+      bankTransferChecked.addEventListener('click', () => {
+         document.getElementById("payment-number-card").value = "";
+         document.getElementById("payment-expiry").value = "";
+         document.getElementById("payment-cvv").value = "";
+      })
+   }
+
+
+   const creditCardLabel = document.querySelector('.credit-card');
+   const bankTransferLabel = document.querySelector('.bank-transfer');
+
+   if (creditCardLabel && bankTransferLabel) {
+      function updateActiveClass(event) {
+         if (event.target.closest('.credit-card')) {
+            creditCardLabel.classList.add('active');
+            bankTransferLabel.classList.remove('active');
+         } else if (event.target.closest('.bank-transfer')) {
+            bankTransferLabel.classList.add('active');
+            creditCardLabel.classList.remove('active');
+         }
+      }
+      creditCardLabel.addEventListener('click', updateActiveClass);
+      bankTransferLabel.addEventListener('click', updateActiveClass);
+   }
+
+
+   /*const paymentRadios = document.querySelectorAll('input[name="payment"]');
+   let isFirstClick = true;
+   function toggleVisibility(radio) {
+      const allWrapDataElements = document.querySelectorAll('.wrap-data');
+      const allTextInfoElements = document.querySelectorAll('.text-info');
+
+      // Hide all wrap-data and text-info elements
+      allWrapDataElements.forEach(element => {
+         element.style.maxHeight = '0';
+         //element.style.padding = '0';
+         element.style.overflow = 'hidden';
+      });
+      allTextInfoElements.forEach(element => {
+         element.style.maxHeight = '0';
+         //element.style.padding = '0';
+         element.style.overflow = 'hidden';
+      });
+
+      // Show the closest wrap-data and text-info elements of the checked radio button
+      if (radio.checked) {
+         const wrapData = radio.closest('label').querySelector('.wrap-data');
+         const textInfo = radio.closest('label').querySelector('.text-info');
+         if (wrapData) {
+            wrapData.style.maxHeight = wrapData.scrollHeight + 'px';
+            //wrapData.style.padding = '10px'; // or whatever padding you need
+            wrapData.style.overflow = 'visible';
+         }
+         if (textInfo) {
+            textInfo.style.maxHeight = textInfo.scrollHeight + 'px';
+            //textInfo.style.padding = '10px'; // or whatever padding you need
+            textInfo.style.overflow = 'visible';
+         }
+      }
+   }
+   paymentRadios.forEach(radio => {
+      radio.addEventListener('change', function () {
+         if (isFirstClick) {
+            isFirstClick = false;
+         }
+         toggleVisibility(this);
+      });
+   });
+   // Trigger change event on page load to handle default checked state
+   const checkedRadio = document.querySelector('input[name="payment"]:checked');
+   if (checkedRadio) {
+      toggleVisibility(checkedRadio);
+   }
+   */
+
+});
